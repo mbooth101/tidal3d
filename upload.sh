d048e56c1657133e49375112bc13620a091a773a
Mat Booth <mat.booth@gmail.com>
2022-07-15 14:03:58 +0100

    Add script for uploading the app to the device for testing

---
+++ upload.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Copy files onto the device using the pyboard tool from micropython,
+# reproduced in this repo for convenience so we don't have to have
+# a local clone available of the firmware or micropython repos:
+# https://github.com/micropython/micropython/blob/master/tools/pyboard.py
+for f in *.py ; do
+	python tools/pyboard.py \
+		--no-soft-reset -d /dev/ttyACM0 -f cp $f :/apps/TiDAL3D/$f
+done
+
+# Monitor serial console
+minicom -D /dev/ttyACM0
