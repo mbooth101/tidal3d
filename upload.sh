86b7dd58c72bf68614f80f50d2b7c694b4cc2179
Mat Booth <mat.booth@gmail.com>
2022-07-19 12:06:26 +0100

    Add external assets for the cube model

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@
 # reproduced in this repo for convenience so we don't have to have
 # a local clone available of the firmware or micropython repos:
 # https://github.com/micropython/micropython/blob/master/tools/pyboard.py
-for f in *.py ; do
+for f in *.py *.obj *.mtl ; do
 	python tools/pyboard.py \
 		--no-soft-reset -d /dev/ttyACM0 -f cp $f :/apps/TiDAL3D/$f
 done
